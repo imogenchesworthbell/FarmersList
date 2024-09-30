163803b66584c258aa88413e7055e47e85c75b76
eloise <eloisewilkinson@115.sub-97-13-190.myvzw.com>
2024-09-30 13:46:41 +0100

    2nd

---
+++ classes/Customer.js
@@ -0,0 +1,13 @@
+class Customer{
+    constructor(name, email, shippingAddress){
+        this.name = name
+        this.email = email
+        this.shippingAddress = shippingAddress
+        this.orderHistory = []
+    }
+    addToOrderHistory(cart){
+        this.cart.push(orderHistory)
+    }
+}
+
+module.exports = Customer;
